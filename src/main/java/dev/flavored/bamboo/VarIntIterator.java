f1196f1613a160862af80a67bb689eacb5c693c6
AppleFlavored <boostelixir@gmail.com>
2024-09-08 16:01:09 -0400

    refactor VarIntIterator into its own class

---
+++ src/main/java/dev/flavored/bamboo/VarIntIterator.java
@@ -0,0 +1,32 @@
+package dev.flavored.bamboo;
+
+import java.util.PrimitiveIterator;
+
+class VarIntIterator implements PrimitiveIterator.OfInt {
+    private final byte[] data;
+    private int offset = 0;
+
+    public VarIntIterator(byte[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public int nextInt() {
+        int value = 0;
+        int size = 0;
+        while (true) {
+            byte b = data[offset + size];
+            value |= (b & 0x7F) << (size++ * 7);
+            if ((b & 0x80) == 0) {
+                break;
+            }
+        }
+        offset += size;
+        return value;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return data.length > offset;
+    }
+}
